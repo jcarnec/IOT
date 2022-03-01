bcba6f89ab8942ae33d17a970c5c3ab571492fb5
Owen <owen.c.johnston@gmail.com>
2022-03-01 18:04:13 +0000

    Added definitions for other functions

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -7,3 +7,33 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+exports.updateGoalIntervalLengthForProject = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Goal interval updated");
+});
+
+exports.getUser = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("User retreived");
+});
+
+exports.takeABreak = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Taking a break");
+});
+
+exports.updateProjectName = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Project name updated");
+});
+
+exports.updateCurrentWorkingProject = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Working project updated");
+});
+
+exports.detectPerson = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Detected person");
+});
